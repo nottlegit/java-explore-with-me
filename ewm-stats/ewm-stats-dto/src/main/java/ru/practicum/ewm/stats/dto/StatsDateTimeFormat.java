c16fe927002c326a60d5800667a707d35b93244f
Grigorii <grromanov.for.training@yandex.ru>
2026-04-01 10:31:49 +0300

    feat: Добавить DTO классы

---
+++ ewm-stats/ewm-stats-dto/src/main/java/ru/practicum/ewm/stats/dto/StatsDateTimeFormat.java
@@ -0,0 +1,8 @@
+package ru.practicum.ewm.stats.dto;
+
+public final class StatsDateTimeFormat {
+    public static final String PATTERN = "yyyy-MM-dd HH:mm:ss";
+
+    private StatsDateTimeFormat() {
+    }
+}
---
+++ ewm-stats/ewm-stats-dto/src/main/java/ru/practicum/ewm/stats/dto/ViewStatsDto.java
@@ -0,0 +1,14 @@
+package ru.practicum.ewm.stats.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+@AllArgsConstructor
+public class ViewStatsDto {
+    private String app;
+    private String uri;
+    private Long hits;
+}
