8e62e5904a714bbe4431992d193dc12eea6a55b8
Grigorii <grromanov.for.training@yandex.ru>
2026-04-20 13:53:58 +0300

    feat: Добавить модуль location

--- main-service/src/main/java/ru/practicum/common/enums/StateActionAdmin.java
+++
@@ -1,6 +0,0 @@
-package ru.practicum.common.enums;
-
-public enum StateActionAdmin {
-    PUBLISH_EVENT,
-    REJECT_EVENT
-}
--- main-service/src/main/java/ru/practicum/location/model/Location.java
+++ main-service/src/main/java/ru/practicum/location/model/Location.java
@@ -1,4 +1,15 @@
 package ru.practicum.location.model;
 
+import jakarta.persistence.Embeddable;
+import lombok.*;
+
+@Embeddable
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Location {
+    private Float lat;
+    private Float lon;
 }
